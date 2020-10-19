344e50891b300fb603568b351b77da9813f83fa9
Kevin Bai <lkevin957@gmail.com>
2020-10-18 21:22:12 -0700

    Add base styling

--- src/Grid/Gridbox/Gridbox.js
+++ src/Grid/Gridbox/Gridbox.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Root = styled.div`
+    height: 5rem;
+    width: 15rem;
+
+    display: grid;
+    padding: 0.5rem;
+    grid-template-columns: 1fr 2fr;
+    grid-template-rows: 1fr;
+
+    align-items: center;
+`;
+
+const TestDiv = styled.div`
+    height: 100%;
+    background: blue;
+`
+
 export const Gridbox = ({
     LeftComponent,
     RightComponent,
     ShouldEmphasizeLeft,
 }) => {
-    debugger;
-
     if (ShouldEmphasizeLeft) {
         return (
-            <div>
-                I am a left emphasized gridbox
-            </div>
+            <Root>
+                <div>
+                    I am a left emphasized gridbox
+                </div>
+                <TestDiv />
+            </Root>
         )
     }
 
