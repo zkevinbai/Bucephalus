1bd3e38baaf1efd7a12a89676a6716d0667930b3
Kevin Bai <lkevin957@gmail.com>
2020-11-03 20:56:28 -0800

    Add base tech lists

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,115 +1,85 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Root = styled.div`
-    display: grid;
-    grid-template-columns: 0.15fr 1fr 0.15fr;
-    grid-template-rows: 0.15fr 1fr 1fr 0.15fr;
-    justify-items: center;
-    align-items: center;
-
-    font-family: "raleway";
-`;
+import GridBox from '../GridBox';
 
 const Title = styled.div`
+    grid-column: 2;
     grid-row: 2;
-    grid-column: 2;
+    align-self: center;
+    text-align: start;
 
-    font-weight: 700;
-    font-size: 39px;
-`;
-
-const Links = styled.div`
-    grid-row: 3;
-    grid-column: 2;
-
+    font-weight: 500;
+    font-size: 35px;
     font-family: "raleway";
-    font-weight: 300;
-    font-size: 25px;
+`;
+const Technologies = styled.div`
+    grid-column: 3;
+    grid-row: 2;
 
-    background: rgba(255, 255, 255, 0.25);
+    background: rgba(255, 255, 255, 0.5);
     border-radius: 15px;
     box-shadow: 0 0 3px;
-
+    
     padding: 5px;
+    margin: 3px;
 
     display: grid;
     grid-template-columns: repeat(4, 1fr);
-    grid-template-rows: 1fr;
-    justify-items: middle;
+    grid-template-rows: repeat(3, 1fr);
+
+    justify-items: center;
+    align-items: end;
 `;
 
-const LinkItem = styled.div`
+const TechnologyItem = styled.div`
+    font-size: 50px;
+
+    background: rgba(255, 255, 255, 1);
+
+    height: 65px;
+    width: 65px;
+    
+    padding: 3px;
+    margin: 2px;
+
+    border-radius: 100%;
+    box-shadow: 0 0 3px;
+
     display: grid;
-    grid-template-columns: 35px 1fr;
-    grid-template-columns: 28px 1fr;
-    grid-template-rows: 1fr;
-
-    color: black;
-
-    :hover {
-        color: rgb(242, 242, 242);
-        color: rgb(86, 194, 255);
-    }
-
-    a:hover{
-        color: rgb(198, 234, 255);
-        color: rgb(86, 194, 255);
-        text-decoration: underline;
-    }
+    align-content: center;
+    justify-content: center;
 `;
 
-const aboutDetails = {
-    itemIcon: 'fas fa-address-card',
-    itemCopy: 'About',
-    itemLink: 'https://nature.berkeley.edu/news/2018/01/student-spotlight-kevin-bai',
-};
-
-const emailDetails = {
-    itemIcon: 'fas fa-paper-plane',
-    itemCopy: 'Email',
-    itemLink: 'mailto:hello@zkevinbai.com',
-};
-
-const githubDetails = {
-    itemIcon: 'fab fa-github',
-    itemCopy: 'Github',
-    itemLink: 'https://github.com/zkevinbai',
-};
-
-const linkedinDetails = {
-    itemIcon: 'fab fa-linkedin',
-    itemCopy: 'Linkedin',
-    itemLink: 'https://www.linkedin.com/in/zkevinbai/',
-};
-
-const SkillsList = [
-    aboutDetails,
-    emailDetails,
-    githubDetails,
-    linkedinDetails,
-];
+const TechnologyList = [
+    'devicon-javascript-plain colored',
+    'devicon-html5-plain-wordmark colored',
+    'devicon-css3-plain-wordmark colored',
+    'devicon-react-original colored',
+    'devicon-d3js-plain colored',
+    'devicon-express-original colored',
+    'devicon-nodejs-plain colored',
+    'devicon-postgresql-plain-wordmark colored',
+]
 
 export const Skills = () => {
     return (
-        <Root>
+        <GridBox
+            ShouldEmphasizeLeft={false}
+        >
             <Title>
-                Full Stack Software Engineer
+                Technologies and Frameworks
             </Title>
-            <Links>
-                {SkillsList.map(({
-                        itemIcon,
-                        itemCopy,
-                        itemLink,
-                    }) => (
-                        <LinkItem>
-                            <i class={itemIcon}/>
-                            <a href={itemLink}>{itemCopy}</a>
-                        </LinkItem>
+            <Technologies>
+                {
+                    TechnologyList.map((icon) => (
+                        <TechnologyItem>
+                            <i class={icon}/>
+                        </TechnologyItem>
                     ))
                 }
-            </Links>
-        </Root>
+            </Technologies>
+        </GridBox>
     );
 }
 
