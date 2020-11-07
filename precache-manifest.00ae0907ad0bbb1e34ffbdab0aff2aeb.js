8ccdc6379e4bdb82edb4846ff4c08eccff35fff7
Kevin Bai <lkevin957@gmail.com>
2020-11-06 22:59:36 -0800

    Updates

---
+++ precache-manifest.00ae0907ad0bbb1e34ffbdab0aff2aeb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2184fb105f4634495bfbf2af29d58b11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ada76e26cedf5dca6f8f",
+    "url": "/static/css/main.5f78809d.chunk.css"
+  },
+  {
+    "revision": "0707c1c55e8f826bf248",
+    "url": "/static/js/2.f37c20d4.chunk.js"
+  },
+  {
+    "revision": "ac27d415db6a8f4ccefe45d91d40e399",
+    "url": "/static/js/2.f37c20d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada76e26cedf5dca6f8f",
+    "url": "/static/js/main.062cc70c.chunk.js"
+  },
+  {
+    "revision": "1a2acea5e8d198b975e7",
+    "url": "/static/js/runtime-main.36982f88.js"
+  },
+  {
+    "revision": "ce11d21fbffe96f6bf4a2c94a9ac9368",
+    "url": "/static/media/Alexandria.ce11d21f.png"
+  },
+  {
+    "revision": "bbc0fa5958eadd7e35706f1cb1be49d5",
+    "url": "/static/media/Augustus.bbc0fa59.png"
+  },
+  {
+    "revision": "a7a59088cb4298bf597bfd70ec6107c7",
+    "url": "/static/media/Aurelian.a7a59088.png"
+  }
+]);
