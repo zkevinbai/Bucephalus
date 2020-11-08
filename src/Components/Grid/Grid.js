2203d3667736be60b66a5fa32fbaa2fd54349570
Kevin Bai <lkevin957@gmail.com>
2020-11-07 19:17:36 -0800

    Add fix to phone screen

--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -5,8 +5,6 @@ const Root = styled.div`
     width: 100vw; 
     height: 100vh;
 
-    overflow: auto;
-
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     grid-template-rows: repeat(3, 1fr);
@@ -19,6 +17,10 @@ const Root = styled.div`
         grid-template-rows: repeat(6, 1fr);
 
         background-image: linear-gradient(to bottom,rgb(255, 210, 206), rgb(255, 254, 180), rgb(183, 229, 255), rgb(255, 210, 206));
+    }
+
+    @media (max-width: 750px) {
+        width: 47rem;
     }
 `;
 
