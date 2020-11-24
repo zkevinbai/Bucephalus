f57b8d00bdf96e8569923f439b7e31fb4f9c5dc2
Kevin Bai <lkevin957@gmail.com>
2020-11-24 11:38:54 -0800

    Add industries

---
+++ src/assets/industries.js
@@ -0,0 +1,30 @@
+Aerospace and Defense
+Agriculture
+Arts, Entertainment, and Recreation
+Automotive
+Construction
+Consulting
+Education
+Energy
+Financial Services and Insurance
+Government and Public Administration
+Health Care
+Hospitality
+Information Services
+Insurance
+IT
+Legal Services
+Life Sciences
+Manufacturing
+Marketing and Advertising
+Non-profit
+Professional Services
+Publishing
+Real Estate, Rental and Leasing
+Retail
+Telecommunications
+Transportation and Warehousing
+Scientific or Technical Services
+Software
+Utilities
+Other
