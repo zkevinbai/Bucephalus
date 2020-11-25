d0c85ccf9d00142aa70a8c5501d6dd515e3b9f9a
Kevin Bai <lkevin957@gmail.com>
2020-11-24 16:46:09 -0800

    Update allposts

--- src/Components/AllPosts/AllPosts.js
+++ src/Components/AllPosts/AllPosts.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import sanityClient from '../../client';
 
 const AllPosts = () => {
+    const [allPostsData, setAllPosts] = useState(null);
+
+    useEffect(() => {
+        sanityClient.fetch(`
+            *[_type == "post"]{
+                title,
+                slug,
+                mainImage{
+                    asset->{
+                        _id,
+                        url
+                    }
+                }
+            }
+        `)
+        .then((data) => setAllPosts(data))
+        .catch(console.error);
+    }, [])
+
     return (
         <div>
             All Posts
+            {
+                allPostsData && 
+                allPostsData.map((post, index) => {
+                    return (
+                        <Link
+                            to={'/' + post.slug.current}
+                            key={post.slug.current}
+                        >
+                            <span key={index}>
+                                <img
+                                    src={post.mainImage.asset.url}
+                                    alt="hero"
+                                />
+                            </span>
+                            <span>
+                                {post.title}
+                            </span>
+                        </Link>
+                    )
+                })
+            }
         </div>
     )
 };
