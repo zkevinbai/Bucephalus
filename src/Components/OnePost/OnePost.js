ed27a8a0e6bd2dc59bf1c55117ee5e598f8623d3
Kevin Bai <lkevin957@gmail.com>
2020-11-26 18:48:54 -0800

    Add image-url

--- src/Components/OnePost/OnePost.js
+++ src/Components/OnePost/OnePost.js
@@ -1,45 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import imageUrlBuilder from '@sanity/image-url';
 
 import sanityClient from '../../client';
 
+const builder = imageUrlBuilder(sanityClient);
+const urlFor = (source) => {
+    return builder.image(source);
+};
+
 const OnePost = () => {
-    const [postData, setPost] = useState(null);
+    const [postData, setPostData] = useState(null);
     const { slug } = useParams();
 
     useEffect(() => {
-        sanityClient.fetch(`
-            *[slug.current == $slug]{
-                title,
-                slug,
-                mainImage{
-                    asset->{
+        sanityClient
+            .fetch(
+                `*[slug.current == $slug]{
+                    title,
+                    slug,
+                    mainImage{
+                        asset->{
                         _id,
                         url
-                    }
-                },
-                body,
-                author{
-                    name,
-                    image{
-                        asset{
-                            _id,
-                            url
                         }
-                    }
-                }
-            }
-        `)
-            .then((data) => setPost(data[0]))
-            .catch(console.error);
+                    },
+                    body,
+                    "name": author->name,
+                    "authorImage": author->image
+                }`,
+            { slug }
+        )
+        .then((data) => setPostData(data[0]))
+        .catch(console.error);
     }, [slug]);
 
-    if (!postData) return <div> loading... </div>; 
+    if (!postData) {
+        return <div> loading... </div>;
+    }
 
     return (
         <div>
             One Post
-            {postData.title}
+            <h2>{postData.title}</h2>
             <img
                 src={postData.author.image.asset.url}
                 alt='Kevin'
