13a072e9cdbcb6985accaedff8a3fa4a6ca4a515
Kevin Bai <lkevin957@gmail.com>
2020-11-26 19:25:50 -0800

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Fragment } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Blog from './Features/Blog';
 import Portfolio from './Features/Portfolio';
 
 function App() {
   return (
-    <Fragment>
-      <Portfolio/>
-    </Fragment>
+    <Router>
+      <Switch>
+        <Route path="/blog">
+          <Blog />
+        </Route>
+        <Route path="/portfolio">
+          <Portfolio />
+        </Route>
+        <Route path="/">
+          <Portfolio />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
