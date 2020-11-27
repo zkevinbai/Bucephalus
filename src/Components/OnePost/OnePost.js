bfb9e86d2819d512321cbd0b2140d7219d3256a7
Kevin Bai <lkevin957@gmail.com>
2020-11-26 18:40:47 -0800

    Add base onepost

--- src/Components/OnePost/OnePost.js
+++ src/Components/OnePost/OnePost.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import sanityClient from '../../client';
 
 const OnePost = () => {
+    const [postData, setPost] = useState(null);
+    const { slug } = useParams();
+
+    useEffect(() => {
+        sanityClient.fetch(`
+            *[slug.current == $slug]{
+                title,
+                slug,
+                mainImage{
+                    asset->{
+                        _id,
+                        url
+                    }
+                },
+                body,
+                author{
+                    name,
+                    image{
+                        asset{
+                            _id,
+                            url
+                        }
+                    }
+                }
+            }
+        `)
+            .then((data) => setPost(data[0]))
+            .catch(console.error);
+    }, [slug]);
+
+    if (!postData) return <div> loading... </div>; 
+
     return (
         <div>
             One Post
+            {postData.title}
+            <img
+                src={postData.author.image.asset.url}
+                alt='Kevin'
+            />
         </div>
     )
 };
