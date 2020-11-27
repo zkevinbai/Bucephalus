687e14676e3e524addea2dbecebc9bf9af154080
Kevin Bai <lkevin957@gmail.com>
2020-11-26 19:20:48 -0800

    Clean up css organization

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Fragment } from 'react';
-import GlobalStyles from './GlobalStyles';
 import Portfolio from './Features/Portfolio';
 
 function App() {
   return (
     <Fragment>
-      <GlobalStyles/>
       <Portfolio/>
     </Fragment>
   );
