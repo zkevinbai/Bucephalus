06ee465718de30b72d1eb360b91172121968ffdc
Kevin Bai <lkevin957@gmail.com>
2020-12-19 12:18:58 -0800

    Update singleblog.js

--- src/Features/Blog/SingleBlog/SingleBlog.js
+++ src/Features/Blog/SingleBlog/SingleBlog.js
@@ -1,28 +1,63 @@
 import React from 'react';
 import styled from 'styled-components';
+import ReactMarkdown from 'react-markdown';
+
+// import myMarkdownFile from 'src/Features/Blog/SingleBlog/OriginalInstructions.md';
+
+// let content;
+
+// fetch(myMarkdownFile)
+//     .then(response => response.text())
+//     .then(text => {
+//         // Logs a string of Markdown content.
+//         // Now you could use e.g. <rexxars/react-markdown> to render it.
+//         content = text;
+//     });
 
 const Root = styled.div`
-    background-image: linear-gradient(to bottom right,rgb(255,150,141), rgb(255,252,103), rgb(86,194,255));
-    display: grid;
-    justify-items: center;
-    align-items: center;
+    margin-left: 20px;
 
     border-radius: 15px;
     margin: 5px;
     border: 5px solid black;
     border-radius: 15px;
 
-    font-family: "lobster";
-    font-size: 85px;
-    /* font-size: 7vw; */
+    width: 200px;
+
+    font-family: "avenir";
+    font-size: 25px;
 `;
 
-export const Name = () => {
+export const SingleBlog = ({
+    markdownContent,
+}) => {
+    const content = `
+  # Header 1
+
+  ---
+
+  ## Header 2
+
+  _italic_
+
+  **bold**
+
+
+  [test](test.com)
+
+
+Close out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+
+Lunch with Mom Dad
+
+  `;
+
     return (
         <Root>
-            Single Blog
+            <ReactMarkdown source={content} />
         </Root>
     );
 }
 
-export default Name;
+export default SingleBlog;
