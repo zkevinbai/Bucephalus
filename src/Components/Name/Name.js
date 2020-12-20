9f7c2128f7f7e31731d5f1fd0840414695331ef9
Kevin Bai <lkevin957@gmail.com>
2020-12-19 23:38:48 -0800

    Add media query for name

--- src/Components/Name/Name.js
+++ src/Components/Name/Name.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { MAX_MOBILE_WIDTH } from '../constants';
+
 const Root = styled.div`
     background-image: linear-gradient(to bottom right,rgb(255,150,141), rgb(255,252,103), rgb(86,194,255));
     display: grid;
@@ -13,8 +15,11 @@ const Root = styled.div`
     border-radius: 15px;
 
     font-family: "lobster";
-    font-size: 85px;
-    /* font-size: 7vw; */
+    font-size: 5.5rem;
+
+    @media(max-width: ${ MAX_MOBILE_WIDTH }) {
+        font-size: 4.2rem;
+    }
 `;
 
 export const Name = () => {
