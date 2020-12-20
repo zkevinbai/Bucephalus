cb79d7a111d8d990e597200599cc8b16a12bd876
Kevin Bai <lkevin957@gmail.com>
2020-12-19 23:55:49 -0800

    project photo media query

--- src/Components/ProjectPhoto/ProjectPhoto.js
+++ src/Components/ProjectPhoto/ProjectPhoto.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { MAX_MOBILE_WIDTH } from '../constants';
+
 const Root = styled.div`
     background: rgba(255, 255, 255, 0.5);
     border-radius: 15px;
     box-shadow: 0 0 3px;
 
-    padding: 10px;
-    margin: 3px;
+    padding: 0.75rem;
+    margin: 0.25rem;
     justify-self: center;
 
     img {
         height: 14rem;
         border-radius: 15px;
         box-shadow: 0 0 3px;
+    }
+
+    @media (max-width: ${MAX_MOBILE_WIDTH}) {
+        padding: 0.50rem;
+        margin: 0rem;
+
+        img {
+            height: 8rem;
+        }
     }
 `;
 
