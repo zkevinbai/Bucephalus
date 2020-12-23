92bc0db6f16b559d0bb7bb47ecc8a6e1204fc34c
Kevin Bai <lkevin957@gmail.com>
2020-12-23 12:57:18 -0800

    Add auto responsive styling to allblogs

--- src/Features/Blog/AllBlogs/AllBlogs.js
+++ src/Features/Blog/AllBlogs/AllBlogs.js
@@ -2,25 +2,40 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Root = styled.div`
-    background-image: linear-gradient(to bottom right,rgb(255,150,141), rgb(255,252,103), rgb(86,194,255));
     display: grid;
-    justify-items: center;
-    align-items: center;
+    grid-template-columns: repeat(auto-fit, minmax(21rem, 1fr));
+        // by setting min to be greater than the max of my elements
+        // the alignment will always be perfect
+`;
+
+const BlogBox = styled.div`
+    background-image: linear-gradient(to bottom right,rgb(255,150,141), rgb(255,252,103), rgb(86,194,255));
 
     border-radius: 15px;
     margin: 5px;
     border: 5px solid black;
     border-radius: 15px;
 
-    font-family: "lobster";
-    font-size: 85px;
-    /* font-size: 7vw; */
+    height: 10rem;
+    width: 20rem;
 `;
 
 export const Name = () => {
     return (
         <Root>
-            All Blog
+            <BlogBox />
+            <BlogBox />
+            <BlogBox />
+            <BlogBox />
+            <BlogBox />
+            <BlogBox />
+            <BlogBox />
+            <BlogBox />
+            <BlogBox />
+            <BlogBox />
+            <BlogBox />
+            <BlogBox />
+            <BlogBox />
         </Root>
     );
 }
