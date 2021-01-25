e65ed8a383f66c2c4b1fe800adec2a39bfbaaab3
Kevin Bai <lkevin957@gmail.com>
2021-01-24 16:22:20 -0800

    Add content module

---
+++ src/Features/Blog/SingleBlog/Content.js
@@ -0,0 +1,75 @@
+// since this is markdown, it must be left aligned to show properly
+export const markdownContent = `
+# This is my blog title
+
+---
+
+## Header 2
+
+_italic_
+
+**bold**
+
+[test](bai)
+
+[hhhhh](j)
+
+> quote
+
+Close out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom Dad
+Close out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+Lunch with Mom DadClose out PR from last night for disabling preview and fixing the IE view of a proposals component.Finish a new ticket to update the SPR sample
+
+`;
+
+export const markdownWithCode = `## How about some code?
+\`\`\`js
+var React = require('react');
+var Markdown = require('react-markdown');
+React.render(
+  <Markdown source="# Your markdown here" />,
+  document.getElementById('content')
+);
+\`\`\``;
+
+// const Content = {
+//     markdownContent,
+//     markdownWithCode,
+// }
+
+// export default Content;
