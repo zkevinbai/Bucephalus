d10cfa06df321cbaf3afcad2a2319bb71c24b59e
Kevin Bai <lkevin957@gmail.com>
2021-01-31 23:45:00 -0800

    simplify single blog

--- src/Features/Blog/SingleBlog/SingleBlog.js
+++ src/Features/Blog/SingleBlog/SingleBlog.js
@@ -8,27 +8,12 @@ import {
     markdownWithCode,
 } from './Content';
 
-// import Content from './Content';
-// const {
-//     markdownContent,
-//     markdownWithCode
-// } = Content;
-
 const Root = styled.div`
-    /* border-radius: 15px;
-    margin: 5px;
-    margin-left: 20px;
-    border: 5px solid black;
-    border-radius: 15px;
-    padding: 1rem; */
-
-    /* width: 50%; */
-    
     height: 100%;
     overflow: hidden;
 
     font-family: "avenir";
-    font-size: 25px;
+    font-size: 1.5rem;
 
     a {
         color: #f55f4e;
